41ec2862f535eebf3df137096a00d6d25a85b009
clarissesoI015-cmyk <clarisseso@gmail.com>
2025-09-18 08:52:27 +0800

    First commit

--- app/config/database.php
+++ app/config/database.php
@@ -61,9 +61,9 @@ $database['main'] = array(
     'driver'	=> 'mysql',
     'hostname'	=> 'sql12.freesqldatabase.com',
     'port'		=> '3306',
-    'username'	=> 'sql12798456',
-    'password'	=> 'UPKqMPdcbP',
-    'database'	=> 'sql12798456',
+    'username'	=> 'sql12798805',
+    'password'	=> 'nGH9jew4sD',
+    'database'	=> 'sql12798805',
     'charset'	=> 'utf8mb4',
     'dbprefix'	=> '',
     // Optional for SQLite
